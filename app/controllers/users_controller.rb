443f8f591205b98cc41a146e11610061da5b16f8
tkori <tkori19+github@gmail.com>
2019-10-11 18:44:02 +0900

    Implement the function of creating a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    user = User.new(user_params)
+    user.save!
+    redirect_to root_path
   end
 
   def show
@@ -15,5 +19,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
