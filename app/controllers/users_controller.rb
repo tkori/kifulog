cb230eae13dfe34871bdf234690b7245d5d79fb6
tkori <tkori19+github@gmail.com>
2019-10-11 20:39:55 +0900

    Add set_user method for refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :delete]
+
   def new
     @user = User.new
   end
@@ -10,21 +12,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     @user.update!(user_params)
     redirect_to @user, notice: "プロフィールが更新されました！" 
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     redirect_to root_path, notice: "ユーザー「#{@user.name}」を削除しました。"
   end
@@ -33,5 +31,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :description)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
