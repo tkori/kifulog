ba491dc6dc0c6772a05c0cf88174c54dec0736ef
tkori <tkori19+github@gmail.com>
2019-10-12 20:59:07 +0900

    Implement Kifus#create

--- app/controllers/kifus_controller.rb
+++ app/controllers/kifus_controller.rb
@@ -6,4 +6,16 @@ class KifusController < ApplicationController
   def new
     @kifu = Kifu.new
   end
+
+  def create
+    kifu = current_user.kifus.new(kifu_params)
+    kifu.save!
+    redirect_to root_path, notice: "棋譜が投稿されました！"
+  end
+
+  private
+
+  def kifu_params
+    params.require(:kifu).permit(:kifu)
+  end
 end
--- app/models/kifu.rb
+++ app/models/kifu.rb
@@ -1,3 +1,4 @@
 class Kifu < ApplicationRecord
   belongs_to :user
+  has_one_attached :kifu
 end
