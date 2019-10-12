bf15dad525bb7dd9c00d72dbe8c9e57e5ca0a31e
tkori <tkori19+github@gmail.com>
2019-10-12 15:16:25 +0900

    Implement Sessions#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :users, except: :index
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
