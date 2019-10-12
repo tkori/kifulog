df276feb67bd368392a839b99ac582ec959778ad
tkori <tkori19+github@gmail.com>
2019-10-12 16:11:05 +0900

    Add kifus paths to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :kifus, only: [:index, :new, :create, :show, :destroy] 
 end
