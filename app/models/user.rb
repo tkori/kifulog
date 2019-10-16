159f3c4133d471a4c87ef5382973d82a6f2865f1
tkori <tkori19+github@gmail.com>
2019-10-16 17:52:25 +0900

    Create associations involved in following function

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :kifus
   has_many :comments
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
 end
